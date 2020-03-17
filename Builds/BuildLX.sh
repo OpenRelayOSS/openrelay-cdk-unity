c9cfff2f2f540385500e1980be039d64d0ab0065
kyadet <kyadet@gmail.com>
2020-03-18 02:23:06 +0900

    fix path

--- Builds/BuildLX.sh
+++ Builds/BuildLX.sh
@@ -3,14 +3,7 @@ unity_version=2019.3.5f1
 unity_path=/opt/Unity/Editor
 unity_exe=${unity_path}/Unity
 build_path=`dirname $0`
-project_path=${build_path}/..
+project_path=./
 target_method=Com.FurtherSystems.OpenRelay.Builds.OpenRelayCDKBuildMenu.BuildALL
 
-ls -l
-ls /opt/Unity/
-echo ${unity_exe} \
- -batchmode \
- -quit \
- -logFile ${build_path}/Build.log \
- -projectPath ${project_path} \
- -executeMethod ${target_method}
+${unity_exe} -batchmode -quit -logFile ${build_path}/Build.log -projectPath ${project_path} -executeMethod ${target_method}
