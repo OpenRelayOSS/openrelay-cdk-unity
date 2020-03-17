806c2b37e0ddc4fd60a93c89c76a572dacf0962c
kyadet <kyadet@gmail.com>
2020-03-18 01:55:37 +0900

    add linux shell unity path

--- Builds/BuildLX.sh
+++ Builds/BuildLX.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 unity_version=2019.3.5f1
-unity_path=/Applications/Unity/Hub/Editor/${unity_version}/Unity.app/Contents/MacOS
+unity_path=/opt/Unity/Editor
 unity_exe=${unity_path}/Unity
 build_path=`dirname $0`
 project_path=${build_path}/..
